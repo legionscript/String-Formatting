515fc60ebcab3e20102ab486429870b7edaf170b
LegionScript <legionscript@legionscript.com>
2023-10-27 10:17:01 -0400

    initial commit

---
+++ tutorial.py
@@ -0,0 +1,27 @@
+# 1. % Operator
+# audience = 'everyone'
+# print('Good morning %s!' % audience)
+
+# person1 = 'Bob'
+# person2 = 'John'
+# print('Good morning %s and %s!' % (person1, person2))
+
+
+
+
+# 2. str.format
+person = 'John'
+person2 = 'Bob'
+
+# print('Hello, {}'.format(person))
+# print('Hello, {} and {}'.format(person, person2))
+
+
+
+
+# 3. String Interpolation / f-strings
+location = 'the office'
+location2 = 'home'
+
+# print(f'You are currently at {location}')
+print(f'Do you want to go to {location} or {location2}')
